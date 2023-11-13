c63b61ea475e751adab261a3165e50cf0dbc5363
Vlad Bibire <vlad@3sidedcube.com>
2023-11-13 11:27:57 +0200

    deployment update

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -35,6 +35,7 @@ const AppProvider = ({ children, client }: AppProviderProps) => {
     useState<AppContextType['isLoggedIn']>(false)
   const [currentUserDetails, setCurrentUserDetails] = useState<User | null>()
 
+  // Deployment
   // Check for Existing Auth Session
   useEffect(() => {
     const getAuthSession = async () => {
